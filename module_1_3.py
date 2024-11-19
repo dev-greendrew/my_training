e911e2330af0611ef0b3eb648ec0802ca869bc65
dev-greendrew <urban.andrey.pythonwork@gmail.com>
2024-11-19 20:49:03 +0700

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,18 @@
+#name = 'Andrey'
+#print(name)
+#age = 19
+#print(age)
+#new_age = 21
+#print(new_age)
+#is_student = True
+#print(is_student)
+#number_completed_tasks = 12
+#number_hours = 1.5
+#name_course = 'Python'
+#time_task = print(number_hours / number_completed_tasks)
+
+#example = 'Middle'
+#print(example[0])
+#print(example[-1])
+#print(example[3:])
+#print(example[1::2])
