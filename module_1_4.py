6292c16af0644d96bdeb363bc21fd009171b08b5
dev-greendrew <urban.andrey.pythonwork@gmail.com>
2024-11-23 18:18:49 +0700

    Update module_1_4.py

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите текст ')
+print("Количество символов:", len(my_string))
+print('Строка в верхнем регистре', my_string.upper())
+print('Строка в нижнем регистре', my_string.lower())
+print('Строка без пробелов', my_string.replace(' ', ''))
+print('Первый символ строки', my_string[0])
+print('Последний символ строки', my_string[-1])
