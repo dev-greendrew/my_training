2daa6d5aec782ff700f15e78e53d55025c940e46
dev-greendrew <urban.andrey.pythonwork@gmail.com>
2024-12-01 21:20:03 +0700

    Update module_1_7Dop.py

--- module_1_7Dop.py
+++ module_1_7Dop.py
@@ -1,1 +1,5 @@
-
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+sorted_students = sorted(students)
+average_grades_dict = {student: sum(grades[i]) / len(grades[i]) for i, student in enumerate(sorted_students)}
+print(average_grades_dict)
